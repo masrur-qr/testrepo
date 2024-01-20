c5f5892a9f44a439726922e5a876f96c345d4548
masrur <you@example.com>
2024-01-20 14:28:43 +0500

    New commit to branch

---
+++ ad.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Server is running")
+
+	http.HandleFunc("/Signup", SignUp)
+	http.HandleFunc("/Login", Login)
+	http.HandleFunc("/New", New)
+
+	http.ListenAndServe(":2222",nil)
+}
+
+func SignUp(w http.ResponseWriter, r *http.Request)  {
+	fmt.Println("Signup")	
+}
+
+func Login(w http.ResponseWriter, r *http.Request)  {
+	fmt.Println("Login")
+}
+
+func New(w http.ResponseWriter, r *http.Request)  {
+	fmt.Println("New")
+}
